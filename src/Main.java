3d27df78d026dce4ce6919c4d759883122eb16f5
Ömür Koca <93597251+jomurKoca@users.noreply.github.com>
2024-02-23 18:21:01 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+
+import java.util.*;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner newscanner = new Scanner(System.in);
+        System.out.println("first side : ");
+        double side1 = newscanner.nextDouble();
+        System.out.println("secound side: ");
+        double side2 = newscanner.nextDouble();
+        double hypotenuse = calculateHypotenuse(side1, side2);
+        newscanner.close();
+        System.out.println("Hypotenus : " + hypotenuse);
     }
+    public static double calculateHypotenuse (double side1 , double side2) {
+        return Math.sqrt(side1 * side1 + side2 * side2);
+    }
+
 }
